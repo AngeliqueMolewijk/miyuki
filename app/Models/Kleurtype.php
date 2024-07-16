f069278a4d301bd76f5989b4b37dcdc1ef41a580
AngelMol <angel1977@zeelandnet.nl>
2024-07-16 10:21:28 +0200

    model

--- app/Models/Kleurtype.php
+++ app/Models/Kleurtype.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class kleurtype extends Model
+class Kleurtype extends Model
 {
     use HasFactory;
     protected $fillable = [
